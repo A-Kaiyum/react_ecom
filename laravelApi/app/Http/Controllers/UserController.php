ffab9d0980935a810d0d838cfbd242308b6f6877
Abdul Kaiyum <52959794+A-Kaiyum@users.noreply.github.com>
2022-02-08 18:01:48 +0600

    react ecommerce crud

---
+++ laravelApi/app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function register(Request $request){
+
+        $data = User::create($request->all());
+        return response()->json($data);
+    }
+}
